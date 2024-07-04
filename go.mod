066b66750afeb3a91bf312f1fe6ee98c714ff78e
Moohomor <makedon.alexandrov@gmail.com>
2024-07-04 17:25:02 +0300

    commit 3.1

--- go.mod
+++ go.mod
@@ -8,6 +8,11 @@ require (
 )
 
 require (
+	github.com/ProtonMail/go-crypto v0.0.0-20230217124315-7d5c6f04bbb8 // indirect
+	github.com/cloudflare/circl v1.3.7 // indirect
+	github.com/dghubble/gologin/v2 v2.5.0 // indirect
+	github.com/google/go-github/v52 v52.0.0 // indirect
+	github.com/google/go-querystring v1.1.0 // indirect
 	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
 	github.com/jackc/pgconn v1.14.3 // indirect
 	github.com/jackc/pgio v1.0.0 // indirect
@@ -17,5 +22,7 @@ require (
 	github.com/jackc/pgtype v1.14.0 // indirect
 	github.com/jackc/puddle v1.3.0 // indirect
 	golang.org/x/crypto v0.20.0 // indirect
+	golang.org/x/oauth2 v0.21.0 // indirect
+	golang.org/x/sys v0.17.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
 )
--- main.go
+++ main.go
@@ -12,9 +12,22 @@ import (
 	"github.com/go-chi/chi"
 	"github.com/jackc/pgx/v4/pgxpool"
 
+	"github.com/dghubble/gologin/v2"
+	"github.com/dghubble/gologin/v2/google"
+	"github.com/dghubble/sessions"
+	"golang.org/x/oauth2"
+	googleOAuth2 "golang.org/x/oauth2/google"
+
 	"memoryflips/handlers"
 	// "hse_school/internal/repositories"
 	// "hse_school/internal/services"
+)
+
+const (
+	sessionName     = "example-google-app"
+	sessionSecret   = "example cookie signing secret"
+	sessionUserKey  = "googleID"
+	sessionUsername = "googleName"
 )
 
 func main() {
@@ -43,6 +56,27 @@ func main() {
 	r.Get("/favicon.ico", handlers.Favicon)
 	// r.Get("/users/{id}", handler.GetUserByID)
 	// r.Post("/users", handler.CreateUser)
+
+	config := &Config{
+		ClientID:     os.Getenv("GOOGLE_CLIENT_ID"),
+		ClientSecret: os.Getenv("GOOGLE_CLIENT_SECRET"),
+	}
+	// allow consumer credential flags to override config fields
+	clientID := flag.String("client-id", "", "Google Client ID")
+	clientSecret := flag.String("client-secret", "", "Google Client Secret")
+	flag.Parse()
+	if *clientID != "" {
+		config.ClientID = *clientID
+	}
+	if *clientSecret != "" {
+		config.ClientSecret = *clientSecret
+	}
+	if config.ClientID == "" {
+		log.Fatal("Missing Google Client ID")
+	}
+	if config.ClientSecret == "" {
+		log.Fatal("Missing Google Client Secret")
+	}
 	
 	// Запуск HTTP сервера
 	log.Println("Starting server on :8080")
@@ -67,4 +101,75 @@ func FileServer(r chi.Router, path string, root http.FileSystem) {
 		fs := http.StripPrefix(pathPrefix, http.FileServer(root))
 		fs.ServeHTTP(w, r)
 	})
+}
+
+var sessionStore = sessions.NewCookieStore[string](sessions.DebugCookieConfig, []byte(sessionSecret), nil)
+
+// Config configures the main ServeMux.
+type Config struct {
+	ClientID     string
+	ClientSecret string
+}
+
+// New returns a new ServeMux with app routes.
+func New(config *Config) *http.ServeMux {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", profileHandler)
+	mux.HandleFunc("/logout", logoutHandler)
+	// 1. Register Login and Callback handlers
+	oauth2Config := &oauth2.Config{
+		ClientID:     config.ClientID,
+		ClientSecret: config.ClientSecret,
+		RedirectURL:  "http://localhost:8080/google/callback",
+		Endpoint:     googleOAuth2.Endpoint,
+		Scopes:       []string{"profile", "email"},
+	}
+	// state param cookies require HTTPS by default; disable for localhost development
+	stateConfig := gologin.DebugOnlyCookieConfig
+	mux.Handle("/google/login", google.StateHandler(stateConfig, google.LoginHandler(oauth2Config, nil)))
+	mux.Handle("/google/callback", google.StateHandler(stateConfig, google.CallbackHandler(oauth2Config, issueSession(), nil)))
+	return mux
+}
+
+// issueSession issues a cookie session after successful Google login
+func issueSession() http.Handler {
+	fn := func(w http.ResponseWriter, req *http.Request) {
+		ctx := req.Context()
+		googleUser, err := google.UserFromContext(ctx)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		// 2. Implement a success handler to issue some form of session
+		session := sessionStore.New(sessionName)
+		session.Set(sessionUserKey, googleUser.Id)
+		session.Set(sessionUsername, googleUser.Name)
+		if err := session.Save(w); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		http.Redirect(w, req, "/profile", http.StatusFound)
+	}
+	return http.HandlerFunc(fn)
+}
+
+// profileHandler shows a personal profile or a login button (unauthenticated).
+func profileHandler(w http.ResponseWriter, req *http.Request) {
+	session, err := sessionStore.Get(req, sessionName)
+	if err != nil {
+		// welcome with login button
+		page, _ := os.ReadFile("home.html")
+		fmt.Fprint(w, string(page))
+		return
+	}
+	// authenticated profile
+	fmt.Fprintf(w, `<p>You are logged in %s!</p><form action="/logout" method="post"><input type="submit" value="Logout"></form>`, session.Get(sessionUsername))
+}
+
+// logoutHandler destroys the session on POSTs and redirects to home.
+func logoutHandler(w http.ResponseWriter, req *http.Request) {
+	if req.Method == http.MethodPost {
+		sessionStore.Destroy(w, sessionName)
+	}
+	http.Redirect(w, req, "/", http.StatusFound)
 }
